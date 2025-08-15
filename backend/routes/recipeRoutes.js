e96b01a06f14fbd5951c5ee2ece3e0e6dbb5afc1
Carmel-Abigail-Shirley-Royan <carmelabigailroyan@gmail.com>
2025-08-15 11:09:53 +0530

    changes

--- backend/routes/recipeRoutes.js
+++ backend/routes/recipeRoutes.js
@@ -4,6 +4,7 @@ const multer = require('multer');
 const path = require('path');
 const fs = require('fs'); // Import the file system module
 const cloudinary = require('cloudinary').v2;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const {
  createRecipe,
  getAllRecipes,
